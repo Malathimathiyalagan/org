19f98f43b1aacc696decc69698c4cc21d939eecd
Malathimathiyalagan <109019668+Malathimathiyalagan@users.noreply.github.com>
2022-07-29 14:29:10 +0530

    Add files via upload

---
+++ org/college/College.java
@@ -0,0 +1,21 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("ABC college");
+
+	}
+	public void collegeCode() {
+	System.out.println("112");	
+
+	}
+	public void collegeRank() {
+		System.out.println("10");
+
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
